f59a3f141bf2ac5dfce3793ed652af7583d55f9b
reddevilmidzy <midzy0228@gmail.com>
2023-09-24 20:26:13 +0900

    feat: 연속하는 중복 문자열 삭제후 결과를 반환

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,49 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Decryption decryption = new Decryption();
+        return decryption.decoding(cryptogram);
+    }
+}
+
+class Decryption {
+
+    public String decoding(String password) {
+        int cnt = password.length()/2;
+        String res = decoded(password);
+
+        while (cnt-->0) {
+            if (password.equals(res)) {
+                break;
+            }
+            password = res;
+            res = decoded(password);
+        }
+
+        return res;
+    }
+
+    private String decoded(String password) {
+        StringBuilder res = new StringBuilder();
+        char pre = ' ';
+        boolean duplicated = false;
+        int n = password.length();
+
+        for (int i = 0; i < n; i++) {
+            if (password.charAt(i) != pre) {
+                if (!duplicated && pre != ' ') {
+                    res.append(pre);
+                }
+                pre = password.charAt(i);
+                duplicated = false;
+            } else {
+                duplicated = true;
+            }
+        }
+        if (!duplicated && pre != ' ') {
+            res.append(pre);
+        }
+
+        return res.toString();
     }
 }
