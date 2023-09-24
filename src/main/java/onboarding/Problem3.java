3f4e32aea6f5c8ce574a36f44c0dca996e7e8ab5
reddevilmidzy <midzy0228@gmail.com>
2023-09-24 21:33:59 +0900

    feat: 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 반환

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        Counter counter = new Counter(new HashSet<>(Arrays.asList('3','6','9')));
+        return counter.count(number);
+    }
+}
+
+class Counter {
+
+    private final HashSet<Character> target;
+
+    public Counter(HashSet<Character> target) {
+        this.target = target;
+    }
+
+    public int count(int number) {
+        int res = 0;
+        for (int i = 1; i <= number; i++) {
+            res += clap(i);
+        }
+        return res;
+    }
+
+    private int clap(int number) {
+        return String.valueOf(number)
+                .chars()
+                .mapToObj(ch -> (char) ch)
+                .filter(target::contains)
+                .mapToInt(ch -> 1) // Count each matching character as 1
+                .sum();
     }
 }
