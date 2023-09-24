65a23d29593e88cbb75d173b4b95a3c4fc766a2f
reddevilmidzy <midzy0228@gmail.com>
2023-09-24 18:50:24 +0900

    feat: 예외 케이스 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,26 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        return compare(pobi, crong);
+        if (isValidate(pobi) && isValidate(crong)) {
+            return compare(pobi, crong);
+        }
+        return -1;
+    }
+
+    public static boolean isValidate(List<Integer> pages) {
+        if (pages.size() != 2) {
+            return false;
+        }
+        if (pages.get(0) % 2 != 1) {
+            return false;
+        }
+        if (pages.get(1) % 2 != 0) {
+            return false;
+        }
+        if (pages.get(1) - pages.get(0) != 1) {
+            return false;
+        }
+        return 1 < pages.get(0) && pages.get(1) < 400;
     }
 
     public static int compare(List<Integer> pobi, List<Integer> crong) {
