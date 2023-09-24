5846febad26386c3ab43b3c5dca9603ca6439649
reddevilmidzy <midzy0228@gmail.com>
2023-09-24 19:09:35 +0900

    refactor: 클래스 분리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,13 +4,54 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        if (isValidate(pobi) && isValidate(crong)) {
-            return compare(pobi, crong);
+        Validator validator = new Validator();
+        Calculator calculator = new Calculator();
+
+        if (validator.isValidate(pobi) && validator.isValidate(crong)) {
+            return calculator.compare(pobi, crong);
         }
         return -1;
     }
+}
 
-    public static boolean isValidate(List<Integer> pages) {
+class Calculator {
+
+    public int compare(List<Integer> pobi, List<Integer> crong) {
+        int pobiVal = maxValue(pobi);
+        int crongVal = maxValue(crong);
+
+        if (pobiVal > crongVal) {
+            return 1;
+        } else if (pobiVal < crongVal) {
+            return 2;
+        }
+        return 0;
+    }
+
+    private int maxValue(List<Integer> pages) {
+        return pages.stream()
+                .mapToInt(val -> Math.max(maxAddValue(val), maxMulValue(val)))
+                .max()
+                .orElse(0);
+    }
+
+    private int maxAddValue(Integer integerVal) {
+        return String.valueOf(integerVal)
+                .chars()
+                .map(Character::getNumericValue)
+                .sum();
+    }
+
+    private int maxMulValue(Integer integerVal) {
+        return String.valueOf(integerVal)
+                .chars()
+                .map(Character::getNumericValue)
+                .reduce(1, (a, b) -> (a * b));
+    }
+}
+
+class Validator {
+    public boolean isValidate(List<Integer> pages) {
         if (pages.size() != 2) {
             return false;
         }
@@ -24,38 +65,5 @@ class Problem1 {
             return false;
         }
         return 1 < pages.get(0) && pages.get(1) < 400;
-    }
-
-    public static int compare(List<Integer> pobi, List<Integer> crong) {
-        int pobiVal = maxValue(pobi);
-        int crongVal = maxValue(crong);
-
-        if (pobiVal > crongVal) {
-            return 1;
-        } else if (pobiVal < crongVal) {
-            return 2;
-        }
-        return 0;
-    }
-
-    private static int maxValue(List<Integer> pages) {
-        return pages.stream()
-                .mapToInt(val -> Math.max(maxAddValue(val), maxMulValue(val)))
-                .max()
-                .orElse(0);
-    }
-
-    private static int maxAddValue(Integer integerVal) {
-        return String.valueOf(integerVal)
-                .chars()
-                .map(Character::getNumericValue)
-                .sum();
-    }
-
-    private static int maxMulValue(Integer integerVal) {
-        return String.valueOf(integerVal)
-                .chars()
-                .map(Character::getNumericValue)
-                .reduce(1, (a, b) -> (a * b));
     }
 }
